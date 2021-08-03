2dedf10089a0791ab98eca0756cc0b304450a671
PxUknDevelopment <88362177+PxUknDevelopment@users.noreply.github.com>
2021-08-03 02:03:30 -0400

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'Nick T. (PxUkn Development)'
+author 'Nick T @ PxUkn Development'
 description 'A chat command, /help will display your servers social links in chat!'
 version '1.0.0'
 
