9cab914d4c161d10cee26fbe343f50652c8bf346
PxUknDevelopment <88362177+PxUknDevelopment@users.noreply.github.com>
2021-08-03 00:20:32 -0400

    Update help.lua

--- help.lua
+++ help.lua
@@ -3,13 +3,13 @@
 
 --When this is exectuted, it will execute a function, doing the msg function to the client
 RegisterCommand('help', function()
-    msg("SRP Discord: discord.signal100roleplay.com")
-    msg("SRP Website: signal100roleplay.com")
-    msg("SRP TeamSpeak: ts.signal100roleplay.com")
-    msg("SRP CAD: cad.signal100roleplay.com")
+    msg("Discord: discord.gg/example")
+    msg("Website: example.com")
+    msg("TeamSpeak: ts.example.com")
+    msg("CAD: cad.example.com")
 end, false)
 
 function msg(text)
     -- TriggerEvent will send a chat message to the client in the prefix as red
-    TriggerEvent("chatMessage",  "[SRP]", {255,0,0}, text)
+    TriggerEvent("chatMessage",  "[Server Name]", {255,0,0}, text)
 end
